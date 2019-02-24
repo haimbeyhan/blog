9a043371daf4900e807977dc96b0844d0254b7f8
Haim Beyhan <haimbeyhan@gmail.com>
2019-02-24 17:20:15 +0200

    changes

--- blog.py
+++ blog.py
@@ -9,7 +9,7 @@ blogposts = [
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Chris Schafer',
+        'author': 'Ralf Schafer',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'November 15, 2018'
